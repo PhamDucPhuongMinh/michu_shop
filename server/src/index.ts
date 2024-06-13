29319a38cbb4ecdc1736085119f2743178735965
phamducphuongminh <phamducphuongminh0103@gmail.com>
2024-06-13 14:22:42 +0700

    BE - Connect MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,29 @@
 import express from 'express'
-const app = express()
-const port = 3000
+import exitHook from 'async-exit-hook'
+import { CLOSE_DB, CONNECT_DB, GET_DB } from './config/mongodb'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const START_SERVER = () => {
+  const app = express()
+  const port = 8080
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.send('Hello World!')
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+  exitHook(() => {
+    console.log('Disconnected to Mongodb Cloud Atlas!')
+    CLOSE_DB()
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connected to Mongodb Cloud Atlas!'))
+  .then(() => START_SERVER())
+  .catch((err) => {
+    console.log(err)
+    process.exit(0)
+  })
